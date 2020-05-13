835f3511d87c9b27dd68a568f3f995f07c5b7c54
edoomm <edoomm8@gmail.com>
2020-05-13 13:03:11 -0500

    P5 Huffman hasta inciso 2 (empezando 3)

---
+++ P5/huffman.py
@@ -0,0 +1,54 @@
+"""
+        INSTITUTO POLITÉCNICO NACIONAL
+        ESCUELA SUPERIOR DE CÓMPUTO
+
+    ANÁLISIS DE ALGORITMOS
+    GRUPO: 3CV2
+    ALUMNOS:
+            - AGUILAR GONZALEZ DANIEL
+            - MENDOZA MARTINEZ EDUARDO
+    PROFESOR: DR. BENJAMÍN LUNA BENOSO
+    PRÁCTICA 5 "DIVIDE Y VENCERAS Y ALGORITMOS VORACES"
+"""
+
+import random
+import heapq # Librearia para cola de prioridad
+
+# Función para generar original.txt (i)
+def generarOriginal(tam, num):
+    # Generando caracteres de manera aleatoria a partir del número de letras que se quiera con num y de tamaño tam
+    text = ''
+    for i in range(tam):
+        text += chr(97 + random.randint(0, num-1))
+
+    # Escribiendo en el archivo
+    with open("original.txt", "w") as file:
+        file.write(text)
+
+    print("original.txt creado")
+
+# Función para generar frecuencias.txt a partir de fileN (ii)
+def generarFrecuencias(fileN):
+    frecuencias = {}
+    with open(fileN, "r+") as file:
+        og = str(file.read())
+        lst = list(set(list(og)))
+        lst.sort()
+        for c in lst:
+            frecuencias[c] = og.count(c)
+
+    with open("frecuencias.txt", "w") as file:
+        file.write(str(frecuencias))
+
+    print("frecuencias.txt creado")
+    return frecuencias
+
+# Función que crea arbol para la codificacion Huffman
+def huffman(freq):
+    n = len(freq.keys())
+    Q = heapq.heapify(freq.values())
+    print(Q)
+
+# Función para generar original_codificado.txt (iii)
+def generarCodificado(fileN):
+    print(generarFrecuencias(fileN))
