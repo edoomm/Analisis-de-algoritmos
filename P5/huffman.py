6e020e062a05b3012e8a7c7d23e26ccba0e7665d
edoomm <edoomm8@gmail.com>
2020-05-16 15:24:26 -0500

    P5 Huffman ahora si terminado xd

--- P5/huffman.py
+++ P5/huffman.py
@@ -11,42 +11,47 @@
     PRÁCTICA 5 "DIVIDE Y VENCERAS Y ALGORITMOS VORACES"
 """
 
-# Ejemplo diapositiva:
-# {'a': 45, 'b': 13, 'c': 12, 'd': 16, 'e': 9, 'f': 5}
+'''
+generarFrecuencias("original.txt")
+generarCodificacion("frecuencias.txt")
+generarCodificado("original.txt", "codificacion.txt")
+generarComprimido("original_codificado.txt")
+generarSalida("original_compr.txt", "codificacion.txt")
+'''
 
 import random
+import math
 import arbolbinario as ab
 import colaprioridad as cp
 import ast # Biblioteca para evaluar strings a dictionaries
 import codecs # Biblioteca para leer 'utf-8'
 
-# Función para generar original.txt (i)
+# Función para generar original.txt (0)
 def generarOriginal(tam, num):
     # Generando caracteres de manera aleatoria a partir del número de letras que se quiera con num y de tamaño tam
     text = ''
     for i in range(tam):
-        text += chr(97 + random.randint(0, num-1))
+        text += chr((random.randint(0, num-1) % 1000000) + 97)
 
     # Escribiendo en el archivo
-    with open("original.txt", "w") as file:
+    with codecs.open("original.txt", encoding="utf-8", mode="w") as file:
         file.write(text)
+# num % 27
 
-    print("original.txt creado")
-
-# Función para generar frecuencias.txt a partir de fileN (ii)
+# Función para generar frecuencias.txt a partir de fileN (i)
 def generarFrecuencias(fileN):
     frecuencias = {}
-    with open(fileN, "r+") as file:
+    with codecs.open(fileN, encoding="utf-8", mode="r") as file:
         og = str(file.read())
         lst = list(set(list(og)))
         lst.sort()
         for c in lst:
             frecuencias[c] = og.count(c)
 
-    with open("frecuencias.txt", "w") as file:
+    with codecs.open("frecuencias.txt", encoding="utf-8", mode="w") as file:
         file.write(str(frecuencias))
 
-    print("frecuencias.txt creado")
+    return frecuencias
 
 # Función que crea arbol para la codificacion Huffman
 def huffman(freq):
@@ -66,36 +71,43 @@ def huffman(freq):
 
     return Q.listaNodos[0]
 
-# Función para generar original_codificado.txt (iii)
-def generarCodificado(fileOg, fileFreq):
-    with open(fileFreq, "r") as file:
+# Función para generar codificacion.txt (ii)
+def generarCodificacion(fileN):
+    with codecs.open(fileN, encoding="utf-8", mode="r") as file:
         freq = ast.literal_eval(file.read())
 
     # Obtenemos la codificacion a partir del algoritmo de Huffman
     arbol = ab.Arbol(huffman(freq))
     codificacion = arbol.obtenerCodificacion()
 
+    with codecs.open("codificacion.txt", encoding="utf-8", mode="w") as file:
+        file.write(str(codificacion))
+
+    return codificacion
+
+# Función para generar original_codificado.txt (iii)
+def generarCodificado(fileOg, fileCod):
     # Generamos el codificado a partir de la codificacion
+    with codecs.open(fileCod, encoding="utf-8", mode="r") as file:
+        codificacion = ast.literal_eval(file.read())
+
     codificado = ""
-    with open(fileOg) as file:
+    with codecs.open(fileOg) as file:
         for line in file: # No es O(n^2) ya que solo se tiene una sola linea en todos los casos, sin embargo así hay que ponerlo en Python
             for c in line:
                 codificado += codificacion[c]
 
-    with open("original_codificado.txt", "w") as file:
+    with codecs.open("original_codificado.txt", encoding="utf-8", mode="w") as file:
         file.write(codificado)
 
-    print("original_codificado.txt creado")
-
-    # Regresa la codificacion, para traduccion
-    return codificacion
+    return codificado
 
 # Función para generar original_compr.txt (iv)
 def generarComprimido(fileN):
     paq = ""
     comprimido = u""
 
-    with open(fileN) as file:
+    with codecs.open(fileN) as file:
         for line in file:
             for i in range(len(line)):
                 paq += line[i]
@@ -114,13 +126,16 @@ def generarComprimido(fileN):
     with codecs.open("original_compr.txt", encoding="utf-8", mode="w") as file:
         file.write(comprimido)
 
-    print("original_compr.txt creado")
+    return comprimido
 
 # Función para mostrar salida de un texto codificado y comprimido a partir de su codificacion (v)
-def generarSalida(fileN, cod):
+def generarSalida(fileComp, fileCod):
+    with codecs.open(fileCod, encoding="utf-8", mode="r") as file:
+        codificacion = ast.literal_eval(file.read())
+
     comprimido = ""
 
-    with codecs.open(fileN, encoding="utf-8") as file:
+    with codecs.open(fileComp, encoding="utf-8") as file:
         for line in file:
             comprimido += line
 
@@ -135,27 +150,60 @@ def generarSalida(fileN, cod):
     salida = ""
     for c in codificado:
         val += c
-        if val in cod.values():
-            salida += list(cod.keys())[list(cod.values()).index(val)]
+        if val in codificacion.values():
+            salida += list(codificacion.keys())[list(codificacion.values()).index(val)]
             val = ""
 
-    with open("salida.txt", "w") as file:
+    with codecs.open("salida.txt", encoding="utf-8", mode="w") as file:
         file.write(salida)
 
-    print("salida.txt creado")
+    return salida
 
 # Función para verificar si dos archivos son iguales
 def archivosIguales(file1, file2):
     txt1 = ""
     txt2 = ""
 
-    with open(file1, "r") as file:
+    with codecs.open(file1, encoding="utf-8", mode="r") as file:
         txt1 = file.read()
 
-    with open(file2, "r") as file:
+    with codecs.open(file2, encoding="utf-8", mode="r") as file:
         txt2 = file.read()
 
     if txt1 == txt2:
         print("Los archivos son iguales(:")
     else:
         print("Los archivos NO son iguales:(")
+
+# Función para generar tabla
+def generarTablaComp(num):
+    print("\nCantidad de bytes usados por", num, "archivos\n")
+    print("\toriginal.txt\t|\toriginal_compr.txt")
+    print("------------------------------------------------")
+    for i in range(num):
+        generarOriginal(random.randint(10, 10 + math.pow(3, i)), random.randint(1, i+5))
+        generarFrecuencias("original.txt")
+        generarCodificacion("frecuencias.txt")
+        generarCodificado("original.txt", "codificacion.txt")
+        generarComprimido("original_codificado.txt")
+
+        numbog = 0
+        with codecs.open("original.txt", encoding="utf-8", mode="r") as file:
+            for line in file:
+                for c in line:
+                    numbog += 1
+
+        numbcomp = 0
+        with codecs.open("original_compr.txt", encoding="utf-8", mode="r") as file:
+            for line in file:
+                for c in line:
+                    numbcomp += 1
+
+
+        if len(str(numbog)) > 5:
+            strnbo = str(numbog) + "\t"
+        else:
+            strnbo = str(numbog) + "\t\t"
+
+
+        print("\t  " + strnbo + "|\t\t" + str(numbcomp))
