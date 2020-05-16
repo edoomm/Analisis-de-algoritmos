947c985b0417e91a4b75f9882b7301dca95a2357
edoomm <edoomm8@gmail.com>
2020-05-16 10:37:51 -0500

    P5 Huffman terminado

--- P5/huffman.py
+++ P5/huffman.py
@@ -17,7 +17,8 @@
 import random
 import arbolbinario as ab
 import colaprioridad as cp
-import ast
+import ast # Biblioteca para evaluar strings a dictionaries
+import codecs # Biblioteca para leer 'utf-8'
 
 # Función para generar original.txt (i)
 def generarOriginal(tam, num):
@@ -84,55 +85,77 @@ def generarCodificado(fileOg, fileFreq):
     with open("original_codificado.txt", "w") as file:
         file.write(codificado)
 
+    print("original_codificado.txt creado")
+
+    # Regresa la codificacion, para traduccion
     return codificacion
 
 # Función para generar original_compr.txt (iv)
 def generarComprimido(fileN):
-    return
+    paq = ""
+    comprimido = u""
 
+    with open(fileN) as file:
+        for line in file:
+            for i in range(len(line)):
+                paq += line[i]
+                if ((i + 1) % 8) == 0:
+                    comprimido += chr(int(paq, 2))
+                    paq = ""
 
+    # Leyendo último paquete de 8 bits y colocando cuantos bits se tomaran de este sobrante
+    s = "," + str(len(paq) % 8)
+    for p in range(len(paq), 8):
+        paq += "0"
 
+    comprimido += chr(int(paq, 2)) + s
 
+    # Escribiendo original_compr.txt
+    with codecs.open("original_compr.txt", encoding="utf-8", mode="w") as file:
+        file.write(comprimido)
 
+    print("original_compr.txt creado")
 
+# Función para mostrar salida de un texto codificado y comprimido a partir de su codificacion (v)
+def generarSalida(fileN, cod):
+    comprimido = ""
 
+    with codecs.open(fileN, encoding="utf-8") as file:
+        for line in file:
+            comprimido += line
 
+    codificado = ""
+    for i in range(0, len(comprimido) - 2):
+        if i != len(comprimido) - 3:
+            codificado += "{0:08b}".format(ord(comprimido[i]))
+        else:
+            codificado += "{0:08b}".format(ord(comprimido[i]))[: int(comprimido[len(comprimido) - 1])]
 
+    val = ""
+    salida = ""
+    for c in codificado:
+        val += c
+        if val in cod.values():
+            salida += list(cod.keys())[list(cod.values()).index(val)]
+            val = ""
 
+    with open("salida.txt", "w") as file:
+        file.write(salida)
 
+    print("salida.txt creado")
 
+# Función para verificar si dos archivos son iguales
+def archivosIguales(file1, file2):
+    txt1 = ""
+    txt2 = ""
 
+    with open(file1, "r") as file:
+        txt1 = file.read()
 
+    with open(file2, "r") as file:
+        txt2 = file.read()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#~Fin
+    if txt1 == txt2:
+        print("Los archivos son iguales(:")
+    else:
+        print("Los archivos NO son iguales:(")
